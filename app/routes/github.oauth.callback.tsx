2c52d842b41a516be07a15b2b88b13e2c08b2124
Stephen Cefali <stephencefali@gmail.com>
2024-06-09 12:54:19 +0800

    adds debug statements

--- app/routes/github.oauth.callback.tsx
+++ app/routes/github.oauth.callback.tsx
@@ -1,4 +1,5 @@
 import { type DataFunctionArgs, redirect } from '@remix-run/node'
+import sqlite3 from 'sqlite3'
 import { GITHUB_LOGIN_URL } from '#app/utils/constants'
 import { prisma } from '#app/utils/db.server.ts'
 import { getMyUser } from '#app/utils/github.ts'
@@ -14,20 +15,48 @@ export async function loader({ request }: DataFunctionArgs) {
 
 	// TODO: better typing
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const tokenResponse: any = await fetch(`https://github.com/login/oauth/access_token`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'Accept': 'application/json'
+	const tokenResponse: any = await fetch(
+		`https://github.com/login/oauth/access_token`,
+		{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+			body: JSON.stringify({
+				client_id: process.env.GITHUB_CLIENT_ID,
+				client_secret: process.env.GITHUB_CLIENT_SECRET,
+				code: code,
+			}),
 		},
-		body: JSON.stringify({
-			client_id: process.env.GITHUB_CLIENT_ID,
-			client_secret: process.env.GITHUB_CLIENT_SECRET,
-			code: code,
-		}),
-	}).then(res => res.json());
-	const { access_token: token, refresh_token: refreshToken, expires_in: expiresIn } = tokenResponse
+	).then(res => res.json())
+	const {
+		access_token: token,
+		refresh_token: refreshToken,
+		expires_in: expiresIn,
+	} = tokenResponse
 	const data: any = await getMyUser({ githubCookie: token })
+
+	console.log('Database URL:', process.env.DATABASE_URL)
+
+	const dbPath = process.env.DATABASE_URL.split(':')[1].split('?')[0]
+	console.log('Database Path:', dbPath)
+
+	// Use sqlite3 to list all tables in the database
+	const db = new sqlite3.Database(dbPath)
+	db.serialize(() => {
+		db.all(
+			"SELECT name FROM sqlite_master WHERE type='table';",
+			(err, tables) => {
+				if (err) {
+					console.error('Error fetching tables:', err)
+				} else {
+					console.log('Tables:', tables)
+				}
+			},
+		)
+	})
+
 	const gitHubUserId = data.id
 
 	const baseGitHubParams = {
